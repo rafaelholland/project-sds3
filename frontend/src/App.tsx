46f4fff1d8b8fe979efbbf2b77263ee594a80b90
rafaelholland <rafael.holland@bandtec.com.br>
2021-05-04 14:57:30 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,19 +8,19 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <h1 className="text-primary py-3">Sales Dashboard</h1>
 
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
+            <h5 className="text-center text-secondary">Success Rate(%)</h5>
             <BarChart/>
           </div>
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <h5 className="text-center text-secondary">All Sales</h5>
             <DonutChart/>
           </div>
         </div>
-        <div className="py-3"><h2 className="text-primary">Todas vendas</h2></div>
+        <div className="py-3"><h2 className="text-primary">All Sales</h2></div>
         <DataTable/>
       </div>
       <Footer/>
