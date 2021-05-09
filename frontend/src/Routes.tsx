7b399ff4ee697373253167ecbb4dda8408a66790
rafaelholland <rafael.holland@bandtec.com.br>
2021-05-09 01:29:20 -0300

    Fix some issues

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,7 +1,6 @@
 import Dashboard from 'pages/Dashboard';
 import Home from 'pages/Home';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import { DiagnosticCategory } from 'typescript';
 
 const Routes = () => {
     return (
